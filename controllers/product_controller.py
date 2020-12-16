579315c24717257c249ac880c95e5e7db5b7a159
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2020-12-16 09:34:08 +0000

    offline_commit_test

--- controllers/product_controller.py
+++ controllers/product_controller.py
@@ -23,3 +23,24 @@ def create_new_product():
     products = product_repository.select_all()
     manufacturers = manufacturer_repository.select_all()
     return render_template("products/new.html", products = products, manufacturers = manufacturers)
+
+# def create_task():
+#     user_id = request.form['user_id']
+#     location_id = request.form['location_id']
+#     review = request.form['review']
+#     user = user_repository.select(user_id)
+#     location = location_repository.select(location_id)
+#     visit = Visit(user, location, review)
+#     visit_repository.save(visit)
+#     return redirect('/visits')
+
+# @visits_blueprint.route("/visits",  methods=['POST'])
+# def create_task():
+#     user_id = request.form['user_id']
+#     location_id = request.form['location_id']
+#     review = request.form['review']
+#     user = user_repository.select(user_id)
+#     location = location_repository.select(location_id)
+#     visit = Visit(user, location, review)
+#     visit_repository.save(visit)
+#     return redirect('/visits')
