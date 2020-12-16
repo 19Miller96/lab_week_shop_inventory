6b435ce3fe8b18fea79aca73260bae3e6ccf5713
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2020-12-16 10:32:22 +0000

    added_the_correct_imports

--- controllers/manufacturer_controller.py
+++ controllers/manufacturer_controller.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
+
+from models.product import Product
 from models.manufacturer import Manufacturer
+
+import repositories.product_repository as product_repository
 import repositories.manufacturer_repository as manufacturer_repository
 
 manufacturers_blueprint = Blueprint("manufacturers", __name__)
@@ -31,8 +35,8 @@ def create_manufacturer():
     manufacturer_repository.save(manufacturer)
     return redirect('/manufacturers')
 
-@products_blueprint.route("/products/new", methods=['GET'])
-def new_product():
-    products = product_repository.select_all()
+@manufacturers_blueprint.route("/manufacturers/new", methods=['GET'])
+def new_manufacturer():
     manufacturers = manufacturer_repository.select_all()
-    return render_template("products/new.html", products = products, manufacturers = manufacturers)
+    products = product_repository.select_all()
+    return render_template("manufacturers/new.html", manufacturers = manufacturers, products = products)
